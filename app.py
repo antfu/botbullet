5bf0e011fe62a0e02e6444000bfde9665a9aad08
Anthony Fu <anthonyfu117@hotmail.com>
2016-09-19 23:02:15 +0800

    Add error handle for module importing

--- app.py
+++ app.py
@@ -15,18 +15,25 @@ def load_configs(path, default_path):
 
 def run():
     configs = load_configs('configs.json','configs_default.json')
-    bot = Bot(configs.api_token, debug=configs.get('debug',False))
+    debug = configs.get('debug',False)
+    bot = Bot(configs.api_token, debug=debug)
     modules = {}
 
     for module_name in configs.modules:
-        module = importlib.import_module('modules.'+module_name)
-        cls = module.export
-        instance = cls()
-        bot.use(instance)
-        modules[module_name] = {"module":module, "cls": cls, "instance": instance}
+        try:
+            if debug: print('Loading module',module_name,'... ', end='')
+            module = importlib.import_module('modules.'+module_name)
+            cls = module.export
+            instance = cls()
+            bot.use(instance)
+            modules[module_name] = {"module":module, "cls": cls, "instance": instance}
+        except Exception as e:
+            if debug: print('Failed  <',str(e),'>')
+        else:
+            if debug: print('OK')
 
     bot.start()
-    input('Start: ')
+    input('Start')
     bot.stop()
 
 if __name__ == '__main__':
